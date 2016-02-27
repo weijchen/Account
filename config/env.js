b741a00d35f7d3e5fa85e18eddc26c71e0ca0138
weijchen <weijchen0919@gmail.com>
2016-02-27 16:02:29 +0800

    add env

---
+++ config/env.js
@@ -0,0 +1,24 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : process.env.CLIENT_ID, // your App ID
+        'clientSecret'  : process.env.CLIENT_SECRET, // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
